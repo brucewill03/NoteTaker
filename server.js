98ac1ac72db5289dd20003dc57ce3008a29b9c20
Bwilliams <brucewill03@yahoo.com>
2022-01-30 16:22:13 -0500

    updated server.js, connected styles.css, created save button for notes

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { emitWarning } = require('process');
+const app = express();
+
+app.use(express.static('Develop/public'));
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// Parse incoming JSON data
+app.use(express.json());
+
+
+app.post('/api/notes', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    fs.readFile('./Develop/db/db.json', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes.push(req.body);
+        console.log(notes);
+
+        fs.writeFile('./Develop/db/db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            console.log('Added note!');
+            res.json(req.body);
+        });   
+    });
+    
+});
+
+
+app.get('/api/notes', (req, res) => {
+
+    fs.readFile('./Develop/db/db.json', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        console.log(notes);
+        res.json(notes);
+    });
+    
+  });
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'));   
+});  
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/public/index.html'));
+});        
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+    
+});
